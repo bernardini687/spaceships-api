93f75b25d0c4bb1730b3b218befed587bec8783d
Oscar Bernardini <bernardini687@gmail.com>
2019-08-19 13:01:30 +0200

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :spaceships
   has_many :spacemen
 
-  act_as_token_authenticatable
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
