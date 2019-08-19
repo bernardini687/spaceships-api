033b8b0be078e4e1c8bf74361384764bd54f31e5
Oscar Bernardini <bernardini687@gmail.com>
2019-08-19 10:00:40 +0200

    Add validations

--- app/models/spaceman.rb
+++ app/models/spaceman.rb
@@ -1,4 +1,9 @@
 class Spaceman < ApplicationRecord
   belongs_to :spaceship
   belongs_to :user
+
+  validates :name, presence: true
+  validates :job, inclusion: {
+    in: %w[captain advisor pilot engineer]
+  }
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,3 +1,10 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :crew, numericality: {
+    only_integer: true,
+    greater_than: 0,
+    less_than: 100
+  }
 end
